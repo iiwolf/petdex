9d98cb7d047ff0641551b1a858199c6641cad14f
Isaac Wolf <ijwolf8@gmail.com>
2022-11-19 10:39:27 -0600

    refactor: Pet as trait

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-struct Pet{
-    name: String,
-    age: f32,
+trait Pet{
+    fn name(&self) -> &'static str;
+    fn age(&self) -> f32;
 }
 
-impl std::fmt::Display for Pet{
+struct Dog{name: &'static str, age: f32}
+
+impl Pet for Dog{
+    fn name(&self) -> &'static str {self.name}
+    fn age(&self) -> f32 {self.age}
+}
+
+impl std::fmt::Display for dyn Pet{
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result{
-        write!(f, "{}, Age {:0.1}", self.name, self.age)
+        write!(f, "{}, Age {:0.1}", self.name(), self.age())
     }
 }
 fn main() {
     println!("Petdex");
 
-    let pet = Pet{name: "Arley".to_string(), age: 4.5};
+    let pet = Dog{name: "Arley", age: 4.5};
     println!("{}", pet);
 }
